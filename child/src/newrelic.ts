5090e9c55fd7aa29d672842bb61f4bfcfb57d817
Farhan <tech_fur@outlook.com>
2025-03-22 09:38:05 +0000

    fix: updated newrelic details

--- child/src/newrelic.ts
+++ child/src/newrelic.ts
@@ -9,11 +9,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['YOUR_APP_NAME'],
+  app_name: ['nextJS-newRelic'],
   /**
    * Your New Relic license key.
    */
-  license_key: 'YOUR_LICENSE_KEY',
+  license_key: 'eeca74c5d4d93c4c83413383cd2fffa9FFFFNRAL',
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
